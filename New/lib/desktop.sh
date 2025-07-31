4e614a0a7c50b137bd76d9a98b2efabfce915378
Roltere <19800085+Roltere@users.noreply.github.com>
2025-07-31 16:07:48 +0100

    Update desktop.sh

--- New/lib/desktop.sh
+++ New/lib/desktop.sh
@@ -13,18 +13,22 @@ install_pacman_tools() {
     done
 }
 
-checkpoint "Gnome desktop and theming"
+checkpoint "Installing GNOME and KDE Plasma desktop environments"
 
-log "Installing Gnome and extras"
+# Install GNOME + core apps
 install_pacman_tools gnome gnome-tweaks kitty thunar firefox pipewire
 
-log "Enabling GDM"
-systemctl enable gdm
+# Install KDE Plasma + core KDE apps + SDDM login manager
+install_pacman_tools plasma kde-applications sddm
 
-log "Applying Gnome settings (keyboard, fonts, etc)"
+log "Enabling SDDM display manager for session switching"
+systemctl enable sddm
+
+# Gnome user config (applies only if session is GNOME)
+log "Applying Gnome keyboard/input settings for user $USERNAME"
 sudo -u "$USERNAME" dbus-launch --exit-with-session gsettings set org.gnome.desktop.input-sources sources "[(\"xkb\", \"gb\")]"
 
-log "Installing noto fonts and fastfetch"
+# Fonts and fastfetch
 install_pacman_tools noto-fonts ttf-noto-nerd fastfetch
 
 log "Downloading wallpaper"
@@ -32,5 +36,6 @@ mkdir -p /home/"$USERNAME"/Pictures
 curl -L https://w.wallhaven.cc/full/rr/wallhaven-rr9kyw.png -o /home/"$USERNAME"/Pictures/wallpaper.png
 chown "$USERNAME:$USERNAME" /home/"$USERNAME"/Pictures/wallpaper.png
 
-checkpoint "Desktop environment setup complete"
+checkpoint "Desktop environments and theming setup complete"
+
 
